589086c6c17d1ecdab22f89eb6458b1868288b85
Viktor Schäfer <blitzm4ch1n3@gmail.com>
2024-07-04 23:25:46 +0200

    Projektdateien hinzufügen.

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LightSwitch.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
+        private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+    }
+}
