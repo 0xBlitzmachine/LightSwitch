d8d5cb4e6823681ae050d5e01b38e5e8c19d16bd
Viktor Schäfer <blitzm4ch1n3@gmail.com>
2024-07-07 20:55:00 +0200

    Test

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,12 +1,19 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 
+using AdonisUI;
+using AdonisUI.Controls;
+
 namespace LightSwitch.Windows
 {
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
+
+        // Header
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
         private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        // Body
     }
 }
