18056d846f3252ef3f93832aad4d065181aaee02
Viktor Schäfer <blitzm4ch1n3@gmail.com>
2024-07-08 10:44:20 +0200

    Remove of AdonisUI + Header Content done.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using AdonisUI;
-using AdonisUI.Controls;
 
 namespace LightSwitch.Windows
 {
-    public partial class MainWindow : AdonisWindow
+    public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+        public MainWindow() => this.InitializeComponent();
+        private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e) => this.DragMove();
     }
 }
