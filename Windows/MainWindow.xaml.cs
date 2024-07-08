1858766c7fc3689adcb3c028939165e3884c00c0
Viktor Schäfer <blitzm4ch1n3@gmail.com>
2024-07-08 17:37:14 +0200

    TabContent - TabItem - TabPanel finished and designed - Ready to implement UserControls

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -6,7 +6,11 @@ namespace LightSwitch.Windows
     public partial class MainWindow : Window
     {
         public MainWindow() => this.InitializeComponent();
+
+        // Header
         private void ExitButton_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
         private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e) => this.DragMove();
+
+        // Body
     }
 }
